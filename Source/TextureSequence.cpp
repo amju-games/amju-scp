fc4eaf6e508d285471b28747d339ece36452083c
jason.e.colman <jason.e.colman@gmail.com>
2011-08-29 10:50:31 +0000

    Fix UVs

--- Source/TextureSequence.cpp
+++ Source/TextureSequence.cpp
@@ -13,13 +13,13 @@ Added to repository
 #include "Common.h"
 #include "TextureSequence.h"
 #include "TextureServer.h"
-#include "SchAssert.h"
+#include "AmjuAssert.h"
+#include <iostream>
 
 namespace Amju
 {
 TextureSequence::TextureSequence()
 {
-  //m_displayList = (unsigned int)-1;
   m_pTexture = 0;
 
   m_numElementsX = 0;
@@ -32,7 +32,6 @@ TextureSequence::TextureSequence()
 
 TextureSequence::~TextureSequence()
 {
-  //glDeleteLists(m_displayList, m_numElements);
 }
 
 int TextureSequence::GetTextureWidth()
@@ -74,36 +73,13 @@ void TextureSequence::Draw(int element)
   Assert(m_pTexture);
   Assert(element < m_numElements);
 
-  // OLD - creating disaply list for individual quads !?!
-//  float dx = 1.0f / (float)m_numElementsX;
-//  float dy = 1.0f / (float)m_numElementsY;
-    
-  //m_displayList = glGenLists(m_numElements);
+  float x = float(element % m_numElementsX);
+  float y = float(element / m_numElementsX);
 
-  //for (int i = 0; i < m_numElements; ++i)
-  //{
-    float x = float(element % m_numElementsX) * m_sizeX;
-    float y = float(element / m_numElementsX) * m_sizeY;
+  x *= m_cellSizeX;
+  y *= m_cellSizeY;
         
-    //glNewList(m_displayList + i, GL_COMPILE);
-    m_pTexture->Bind();
-    //glBegin(GL_QUADS);
-/*
-    glTexCoord2f(x, 1.0f - y - dy);
-    glVertex2f(0, 0);
-    
-    glTexCoord2f(x + dx, 1.0f - y - dy);
-    glVertex2f(sizeX, 0);
-    
-    glTexCoord2f(x + dx, 1.0f - y);
-    glVertex2f(sizeX, sizeY);
-    
-    glTexCoord2f(x, 1.0f - y);
-    glVertex2f(0, sizeY);
-        
-    //glEnd();
-    //glEndList();
-*/
+  //m_pTexture->Bind();
 
   AmjuGL::Vert verts[] = 
   {
@@ -125,14 +101,11 @@ void TextureSequence::Draw(int element)
   tri->m_verts[1] = verts[2];
   tri->m_verts[2] = verts[3];
 
-  AmjuGL::Disable(AmjuGL::AMJU_DEPTH_READ); // TODO have a member flag
-  AmjuGL::Disable(AmjuGL::AMJU_LIGHTING);
-  AmjuGL::PushMatrix();
-//  AmjuGL::MultMatrix(m_combined); // NB combined
-//  m_texture->UseThisTexture();
+//  AmjuGL::PushAttrib(AmjuGL::AMJU_DEPTH_READ | AmjuGL::AMJU_LIGHTING);
+//  AmjuGL::Disable(AmjuGL::AMJU_DEPTH_READ); // TODO have a member flag ?
+//  AmjuGL::Disable(AmjuGL::AMJU_LIGHTING);
   AmjuGL::DrawTriList(tris);
-  AmjuGL::PopMatrix();
-  AmjuGL::Enable(AmjuGL::AMJU_DEPTH_READ);
+//  AmjuGL::PopAttrib();
 
   return;
 }
@@ -141,8 +114,6 @@ int TextureSequence::GetNumElements() const
 {
   return m_numElements;
 }
-
-
 
 void TextureSequence::Bind()
 {
