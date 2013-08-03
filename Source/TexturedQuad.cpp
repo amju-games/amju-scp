f23e46923ab4697ca2db6eaaa924115f215897bf
jason.e.colman <jason.e.colman@gmail.com>
2013-08-03 13:50:40 +0000

    Use TriList for drawing

--- Source/TexturedQuad.cpp
+++ Source/TexturedQuad.cpp
@@ -1,21 +1,3 @@
-/*
-Amju Games source code (c) Copyright Jason Colman 2004
-$Log: TexturedQuad.cpp,v $
-Revision 1.1.10.2  2005/06/05 00:23:23  Administrator
-Scene Ed fix
-
-Revision 1.1.10.1  2005/03/31 22:53:16  jay
-Added Save() for SCENE_EDITOR
-
-Revision 1.1  2004/09/08 15:42:59  jay
-Added to repository
-  
-*/
-
-#if defined(WIN32)
-#pragma warning(disable: 4786)
-#endif
-
 #include "TexturedQuad.h"
 #include "TextureServer.h"
 #include "TextWriter.h"
@@ -106,10 +88,6 @@ void TexturedQuad::Draw(float top, float left, float bottom, float right)
   {
     return;
   }
-//  AmjuGL::PushMatrix();
-//  AmjuGL::PushAttrib(AmjuGL::AMJU_LIGHTING | AmjuGL::AMJU_DEPTH_READ | AmjuGL::AMJU_BLEND);
-    
-//  AmjuGL::Translate(0, 0, TextWriter::Z_OFFSET);
 
   float xmax = right * TextWriter::CHAR_SIZE - TextWriter::X_OFFSET;
   float xmin = left * TextWriter::CHAR_SIZE - TextWriter::X_OFFSET;
@@ -118,38 +96,7 @@ void TexturedQuad::Draw(float top, float left, float bottom, float right)
 
   AmjuGL::Disable(AmjuGL::AMJU_DEPTH_READ);
 
-  ////////glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); 
-//  AmjuGL::Enable(AmjuGL::AMJU_BLEND);
-
-//  AmjuGL::Disable(AmjuGL::AMJU_LIGHTING);
-
   m_pTex->Bind();
-
-  // TODO
-/*
-  glBegin(GL_QUADS);
-
-    float y1 = 1.0f - m_areaBottom; // usually == 0
-    float y2 = 1.0f - m_areaTop; // usually == 1.0
-
-    //glTexCoord2f(0, 0);
-    // Now we can specify an area of the texture to draw.
-    glTexCoord2f(m_areaLeft, y1);
-    glVertex3f(xmin, ymin, 0);
-    
-    //glTexCoord2f(1.0f, 0);	
-    glTexCoord2f(m_areaRight, y1);
-    glVertex3f(xmax, ymin, 0);
-    
-    //glTexCoord2f(1.0f, 1.0f);
-    glTexCoord2f(m_areaRight, y2);
-    glVertex3f(xmax, ymax, 0);
-    
-    //glTexCoord2f(0, 1.0f);
-    glTexCoord2f(m_areaLeft, y2);
-    glVertex3f(xmin, ymax, 0);
-  glEnd();
-*/
 
   float y1 = 1.0f - m_areaBottom; // usually == 0
   float y2 = 1.0f - m_areaTop; // usually == 1.0
@@ -174,10 +121,13 @@ void TexturedQuad::Draw(float top, float left, float bottom, float right)
   tri.m_verts[1] = verts[2];
   tri.m_verts[2] = verts[3];
   tris.push_back(tri);
-  AmjuGL::DrawTriList(tris);
-
-//  AmjuGL::PopAttrib();
-//  AmjuGL::PopMatrix();
+  
+  if (!m_triList)
+  {
+    m_triList = (TriListDynamic*)AmjuGL::Create(TriListDynamic::DRAWABLE_TYPE_ID);
+  }
+  m_triList->Set(tris);
+  AmjuGL::Draw(m_triList);
 }
 
 void TexturedQuad::SetDrawArea(float t, float l, float b, float r)
